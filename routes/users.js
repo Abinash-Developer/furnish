f44be6e4129880bd6bb5c55ed671ccb7d9ab422a
Abinash <abinashswain45678@gmail.com>
2026-03-29 20:46:26 +0530

    fix ssh

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/login", (req, res) => {
-  res.json({message:"Login"});
+  res.json({message:"Logins"});
 });
 
 router.post("/register", (req, res) => {
-  res.json({message:"Register"});
+  res.json({message:"Registers"});
 });
 
 module.exports = router;
