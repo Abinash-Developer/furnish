c3bf65281d94c973ade7fc64772aa323b01503bf
Abinash <abinashswain45678@gmail.com>
2026-03-29 17:04:36 +0530

    final-test-deploy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Basic Route
 app.get('/', (req, res) => {
-  res.json({ message: 'Server is running!',status:"Abinash Abinash Abinash" });
+  res.json({ message: 'Server is running!',status:"Abinash Abinash" });
 });
 
 // Your Routes here
